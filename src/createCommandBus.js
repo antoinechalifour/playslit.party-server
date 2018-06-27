ad3015ffee2bf97e0175b2ea46832a6a2890d0b9
antoinechalifour <antoine.chalifour@gmail.com>
2018-06-27 21:33:58 +0200

    feat(CreateParty): Use validators

--- src/createCommandBus.js
+++ src/createCommandBus.js
@@ -1,12 +1,26 @@
+// Middlewares
 const Dispatcher = require('./Commands/Dispatcher')
 const EventBusDispatcherMiddleware = require('./Commands/middlewares/EventDispatcher')
 const LoggerMiddleware = require('./Commands/middlewares/LoggerMiddleware')
+
+// Utils
+const ValidatorDecorator = require('./Commands/decorators/Validator')
+
+// Handles
 const CreatePartyHandler = require('./Commands/CreateParty/Handler')
+const CreatePartyPayloadValidator = require('./Commands/CreateParty/validators/Payload')
+const PartyShouldNotExistValidator = require('./Commands/CreateParty/validators/PartyShouldNotExist')
+
 const DeletePartyHandler = require('./Commands/DeletePartyCommand/Handler')
+
 const JoinPartyHandler = require('./Commands/JoinParty/Handler')
+
 const LeavePartyHandler = require('./Commands/LeaveParty/Handler')
+
 const SendSignalingAnswerHandler = require('./Commands/SendSignalingAnswer/Handler')
+
 const SendSignalingCandidateHandler = require('./Commands/SendSignalingCandidate/Handler')
+
 const SendSignalingOfferHandler = require('./Commands/SendSignalingOffer/Handler')
 
 module.exports = function createCommandBus ({
@@ -16,7 +30,13 @@ module.exports = function createCommandBus ({
   userRepository
 }) {
   const handlers = [
-    new CreatePartyHandler(userRepository, partyRepository),
+    new ValidatorDecorator(
+      new ValidatorDecorator(
+        new CreatePartyHandler(userRepository, partyRepository),
+        new PartyShouldNotExistValidator(partyRepository)
+      ),
+      new CreatePartyPayloadValidator()
+    ),
     new DeletePartyHandler(userRepository, partyRepository),
     new JoinPartyHandler(tokenService, partyRepository, userRepository),
     new LeavePartyHandler(userRepository),
